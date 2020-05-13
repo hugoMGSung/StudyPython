b0b341464efc07dc51d3ef75584a6e7b6613eed8
hugoMGSung <personar95@naver.com>
2020-05-13 16:23:51 +0900

    5-3 커밋

--- chap5_1.py
+++ chap5_1.py
@@ -21,6 +21,19 @@ def print_n_times(*values, n=3):
 def func(a, b=10, c=20):
     print(a + b + c)
 
+def return_test():
+    print("Value A")
+    return
+    #print("Value B")
+
+def multi_all(start, end):
+    output = 1
+
+    for i in range(start, end + 1):
+        output *= i
+    
+    return output
+
 # print_3_times()
 #print_n_times("Hello", "World", n=2)
 # print_n_times(2, "안녕하세요", "즐거운", "파이썬", "프로그래밍")
@@ -28,4 +41,15 @@ def func(a, b=10, c=20):
 #print_n_times("안녕하세요", "즐거운", "파이썬", "프로그래밍", n=1)
 
 # func(10)
-func(c=100, a=50, b=50) #이렇게 쓰지 맙시다
+# func(c=100, a=50, b=50) #이렇게 쓰지 맙시다
+
+#value = return_test()
+value = None
+#print("value 는 {}".format(value))
+if (value == None):
+    print("value 값이 없습니다")
+else:
+    print("value 는 {}".format(value))
+
+print("1 to 100", multi_all(1, 100))
+print("1 to 1000", multi_all(1, 1000))
--- format_str.py
+++ format_str.py
@@ -1,5 +1,5 @@
-import locale
-
+#import locale
 # print(locale.setlocale(locale.LC_ALL, "ko_KR"))
-print(locale.format("%d", 1235000, grouping=True))
-# print(format(1235000, ',d'))
+# print(locale.format("%d", 1235000, grouping=True))
+print(format(350000000, ',d'))
+#print(locale.format_string('%d', 1235000, grouping=True))
